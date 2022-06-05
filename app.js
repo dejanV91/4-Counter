60cf04f12f5b032e8cd5290da928774ff762ae91
dejanV91 <dejanvukoicic91@gmail.com>
2022-06-05 18:55:49 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const counter=0;
+
+
+
+const countView=document.getElementById("value");
+const decreaseBtn= document.getElementsByClassName("decrease");
+const resetBtn= document.getElementsByClassName("reset");
+const increaseBtn= document.getElementsByClassName("increase");
+
+// while(true){
+//     if (decreaseBtn.onclick) {
+//         counter--
+//         countView.textContent(counter);
+//     }
+// }
