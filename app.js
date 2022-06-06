c3385c13b863137baa7f71d0a1cf1f6cb02fb72f
dejanV91 <dejanvukoicic91@gmail.com>
2022-06-06 18:09:05 +0200

    initial commit

--- app.js
+++ app.js
@@ -1,15 +1,45 @@
-const counter=0;
-
-
+let counter=0;
 
 const countView=document.getElementById("value");
 const decreaseBtn= document.getElementsByClassName("decrease");
 const resetBtn= document.getElementsByClassName("reset");
 const increaseBtn= document.getElementsByClassName("increase");
 
-// while(true){
-//     if (decreaseBtn.onclick) {
-//         counter--
-//         countView.textContent(counter);
-//     }
-// }
+decreaseBtn[0].addEventListener("click",function(){
+    counter--;
+    countView.textContent= counter;
+    if (counter > 0) {
+        countView.style.color = "green";
+    } else if (counter < 0) {
+        countView.style.color = "red";
+    } else {
+        countView.style.color = "black";
+    }
+    
+})
+
+increaseBtn[0].addEventListener("click",function(){
+    counter++;
+    countView.textContent= counter;
+    if (counter > 0) {
+        countView.style.color = "green";
+    } else if (counter < 0) {
+        countView.style.color = "red";
+    } else {
+        countView.style.color = "black";
+    }
+})
+
+resetBtn[0].addEventListener("click",function(){
+    counter=0;
+    countView.textContent= counter;
+    if (counter > 0) {
+        countView.style.color = "green";
+    } else if (counter < 0) {
+        countView.style.color = "red";
+    } else {
+        countView.style.color = "black";
+    }
+})
+
+
