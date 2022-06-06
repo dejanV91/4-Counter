20faba928e07ef39fd300b4720acc34f4ed4811a
dejanV91 <dejanvukoicic91@gmail.com>
2022-06-06 20:03:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Counter
+
+Counter with JavaScript
+
+## Stack:
+* Language: **HTML5**, **CSS3**, **JavaScript**
--- app.js
+++ app.js
@@ -1,12 +1,29 @@
 let counter=0;
 
-const countView= document.querySelector('#value');
-const buttons=document.querySelectorAll('.btn');
+const countView= document.querySelector("#value");
+const buttons=document.querySelectorAll(".btn");
 
 buttons.forEach(function(singleBtn){
-    singleBtn.addEventListener("click" ,function(a){
-        let currentBtn=a.currentTarget;
+    singleBtn.addEventListener("click" ,function(j){
+        const currentBtn=j.currentTarget.classList;
         
+        if (currentBtn.contains("increase")) {
+            counter++;
+        } else if (currentBtn.contains("decrease")) {
+            counter--;
+        } else {
+            counter=0;
+        }
+
+        if (counter > 0) {
+            countView.style.color = "green";
+        } else if (counter < 0) {
+            countView.style.color = "red";
+        } else {
+            countView.style.color = "black";
+        }
+
+        countView.textContent = counter;
     })
     
 });
